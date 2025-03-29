38a7aadc735baa090bdb285c69f33662522830b5
Muhab <swe.muhabfawzy@gmail.com>
2025-03-30 01:02:28 +0200

    Update app.js

--- api/app.js
+++ api/app.js
@@ -26,6 +26,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(express.bodyParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
